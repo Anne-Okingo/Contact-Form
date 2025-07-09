94af970ba6bfdb5fc313234a5d5e744ff75d9627
Anne-Okingo <annekadiso@gmail.com>
2025-07-09 16:05:18 +0300

    feat: Initialize App component with main layout and heading

---
+++ contact-form/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <main style={{ fontFamily: "Arial", maxWidth: "600px", margin: "2rem auto" }}>
+      <h1>Contact Me</h1>
+      <p>Fill out the form below and I’ll get back to you!</p>
+    </main>
+  );
+}
+
+export default App;
